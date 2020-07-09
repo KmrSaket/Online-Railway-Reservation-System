975abb7d6d8fa6e9c5b3938f1dd2e576ce3fe36b
Saket Kumar <saket.kumar.null@gmail.com>
2020-07-09 15:05:39 +0530

    DB updated

--- database/orrs.sql
+++ database/orrs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 09, 2020 at 08:04 AM
+-- Generation Time: Jul 09, 2020 at 09:30 AM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.14
 
